49fd06d43814f562c37b0480013fb305e21ed6e4
harsh pratap <harsh.singh@visionias.in>
2024-04-27 15:14:44 +0530

    added multiprocessing feature

--- transcription_scripts/handler.py
+++ transcription_scripts/handler.py
@@ -1,4 +1,6 @@
 import sys
+import multiprocessing
+
 from fetch_embed_code import get_id_from_embed_code
 from download_video import (
     get_signed_url,
@@ -23,32 +25,41 @@ from delete_files import delete_files_from_local
 
 from upload_on_s3 import upload_transcript_subtitle_to_s3
 
-class_id = sys.argv[1]
-embed_code_id = get_id_from_embed_code(class_id=class_id)
-signed_url = get_signed_url(embed_code_id=embed_code_id)
 
-if not signed_url:
-    print("No videos exist on GCP for the given class id")
-else:
-    download_file(signed_url=signed_url, class_id=class_id)
-    write_audio_file(class_id=class_id)
-    print("loading audio file.....")
-    audio = load_audio_file(class_id=class_id)
-    print("decoding audio file.....")
-    transcript = decode_audio_file(audio=audio)
-    print("writing transcription file.....")
-    write_transcription_file(transcript_text=transcript['text'], class_id=class_id)
-    print("writing srt file.....")
-    write_srt_file(transcript=transcript, class_id=class_id)
-    print("loading transcription file.....")
-    pages = load_text_file(class_id=class_id)
-    print("splitting transcription file.....")
-    docs = recursive_text_splitter(pages)
-    print("embedding splits.....")
-    db = embed_data(docs)
-    print("updating transcription status in db.....")
-    update_transcription_status(class_id=class_id)
-    print("uploading transcription and subtitle files to s3.....")
-    upload_transcript_subtitle_to_s3(class_id=class_id)
-    print("deleting files from local memory..............")
-    delete_files_from_local(class_id=class_id)
+def transcribe_video(class_id):
+    embed_code_id = get_id_from_embed_code(class_id=class_id)
+    signed_url = get_signed_url(embed_code_id=embed_code_id)
+    if not signed_url:
+        print("No videos exist on GCP for the given class id")
+    else:
+        print(f"Transcribing video {class_id}...")
+        download_file(signed_url=signed_url, class_id=class_id)
+        write_audio_file(class_id=class_id)
+        audio = load_audio_file(class_id=class_id)
+        print(f"decoding audio file for {class_id}.....")
+        transcript = decode_audio_file(audio=audio)
+        print(f"writing transcription file for.....{class_id}")
+        write_transcription_file(transcript_text=transcript['text'], class_id=class_id)
+        write_srt_file(transcript=transcript, class_id=class_id)
+        # print("loading transcription file.....")
+        pages = load_text_file(class_id=class_id)
+        # print("splitting transcription file.....")
+        docs = recursive_text_splitter(pages)
+        # print("embedding splits.....")
+        db = embed_data(docs)
+        # print("updating transcription status in db.....")
+        update_transcription_status(class_id=class_id)
+        # print("uploading transcription and subtitle files to s3.....")
+        upload_transcript_subtitle_to_s3(class_id=class_id)
+        # print("deleting files from local memory..............")
+        delete_files_from_local(class_id=class_id)
+        print(f"Transcription for video {class_id} complete.")
+
+
+if __name__ == "__main__":
+    # List of class_ids for the videos to transcribe
+    class_ids = sys.argv[1:]
+
+    # Create a multiprocessing pool
+    with multiprocessing.Pool() as pool:        # Map the transcribe_video function to each class_id in the pool
+        pool.map(transcribe_video, class_ids)
