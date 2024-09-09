c791506e61e5345c65995402aee6e4e22985a708
harsh <harsh.singh@visionias.in>
2024-09-09 18:06:47 +0530

    changed embedding model

--- transcription_scripts/customEmbeddings.py
+++ transcription_scripts/customEmbeddings.py
@@ -3,8 +3,8 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 
 class CustomOpenAIEmbeddings(OpenAIEmbeddings):
 
-    def __init__(self, openai_api_key, *args, **kwargs):
-        super().__init__(openai_api_key=openai_api_key, *args, **kwargs)
+    def __init__(self, openai_api_key, model='text-embedding-3-large', *args, **kwargs):
+        super().__init__(openai_api_key=openai_api_key, model=model, *args, **kwargs)
 
     def _embed_documents(self, texts):
         return super().embed_documents(texts)
